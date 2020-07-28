cdd82728caa55d473a3f46e6176672ffff72e61d
JuliusVillagracia <villagracia.juliusdarwin@gmail.com>
2020-07-28 18:16:36 +0800

    Installed Tailwind and dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	render() {
+		return (
+			<header className="border-b p-3 flex justify-between items-center">
+				<span className="font-bold">
+					Site
+				</span>
+			</header>
+		);
+	}
 }
 
 export default App;
