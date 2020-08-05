996dbb5018943987561d27d045b2ed560369f3dd
JuliusVillagracia <villagracia.juliusdarwin@gmail.com>
2020-08-05 18:52:35 +0800

    Created menu schema in the backend

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,12 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 
+let Menu = require('./menu.model');
+
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/delivery', { useNewUrlParser: true });
+mongoose.connect('mongodb://127.0.0.1:27017/menu', { useNewUrlParser: true });
 const connection = mongoose.connection;
 
 mongoose.connection.once('open', function () {
